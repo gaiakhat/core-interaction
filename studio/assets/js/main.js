2ce0bf470be03240081b70460541dbe00ea4c280
gaiakhat <khatg499@newschool.edu>
2018-04-05 15:27:01 -0400

    [some message]

--- studio/assets/js/main.js
+++ studio/assets/js/main.js
@@ -1,1 +1,17 @@
+var description = document.querySelector('.description')
+var posterList = document.querySelector('.poster-list')
 
+var highlight1 = document.querySelector('.highlight.one')
+var poster1 = document.querySelector('.poster.one')
+
+highlight1.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster1.classList.add('is-active')
+})
+
+highlight1.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster1.classList.remove('is-active')
+})
