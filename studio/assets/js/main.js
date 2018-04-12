f15200f47277e7f1ed1a07f598e770ade3b5d0b8
gaiakhat <khatg499@newschool.edu>
2018-04-12 09:24:45 -0400

    [some message]

--- studio/assets/js/main.js
+++ studio/assets/js/main.js
@@ -1,6 +1,8 @@
 var description = document.querySelector('.description')
 var posterList = document.querySelector('.poster-list')
 
+// POSTER 1
+
 var highlight1 = document.querySelector('.highlight.one')
 var poster1 = document.querySelector('.poster.one')
 
@@ -14,4 +16,89 @@ highlight1.addEventListener('mouseout', function() {
 	description.classList.remove('is-active')
 	posterList.classList.remove('is-active')
 	poster1.classList.remove('is-active')
+})
+
+// POSTER 2
+
+var highlight2 = document.querySelector('.highlight.two')
+var poster2 = document.querySelector('.poster.two')
+
+highlight2.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster2.classList.add('is-active')
+})
+
+highlight2.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster2.classList.remove('is-active')
+})
+
+// POSTER 3
+
+var highlight3 = document.querySelector('.highlight.three')
+var poster3 = document.querySelector('.poster.three')
+
+highlight3.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster3.classList.add('is-active')
+})
+
+highlight3.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster3.classList.remove('is-active')
+})
+
+// POSTER 4
+
+var highlight4 = document.querySelector('.highlight.four')
+var poster4 = document.querySelector('.poster.four')
+
+highlight4.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster4.classList.add('is-active')
+})
+
+highlight4.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster4.classList.remove('is-active')
+})
+
+// POSTER 5
+
+var highlight5 = document.querySelector('.highlight.five')
+var poster5 = document.querySelector('.poster.five')
+
+highlight5.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster5.classList.add('is-active')
+})
+
+highlight5.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster5.classList.remove('is-active')
+})
+
+// POSTER 6
+
+var highlight6 = document.querySelector('.highlight.six')
+var poster6 = document.querySelector('.poster.six')
+
+highlight6.addEventListener('mouseover', function() {
+	description.classList.add('is-active')
+	posterList.classList.add('is-active')
+	poster6.classList.add('is-active')
+})
+
+highlight6.addEventListener('mouseout', function() {
+	description.classList.remove('is-active')
+	posterList.classList.remove('is-active')
+	poster6.classList.remove('is-active')
 })
