ecdf75b7aa0842e895fb75964a74fbff718a6a64
gaiakhat <khatg499@newschool.edu>
2018-05-10 16:55:34 -0400

    [some message]

--- studio/assets/js/main.js
+++ studio/assets/js/main.js
@@ -1,11 +1,17 @@
 var highlights = Array.from(document.querySelectorAll('.highlight'));
-var posterList = document.querySelector('.poster-list');
-var description = document.querySelector('.description');
+var posterLists = Array.from(document.querySelectorAll('.poster-list'));
+var descriptions = Array.from(document.querySelectorAll('.description'));
 
 highlights.forEach(function(highlight) {
+  // get the ID of the .highlight element
   var id = highlight.dataset.id;
+
+  // find the corresponding .poster-wrapper element that has a matching data-id
   var posterDiv = document.querySelector('.poster-wrapper[data-id=' + id + ']');
+
+  // find the corresponding .info element that has a matching data-id
   var infoDiv = document.querySelector('.poster-wrapper[data-id=' + id + '] .info');
+  console.log()
 
   fetch('https://www.omdbapi.com/?apikey=6d6fa250&i=' + id)
     .then(function(response) {
@@ -21,14 +27,32 @@ highlights.forEach(function(highlight) {
     });
 
   highlight.addEventListener('mouseover', function() {
-    description.classList.add('is-active');
-    posterList.classList.add('is-active');
+    // add -is-active classs to all .description elements
+    descriptions.forEach( function(description) {
+      description.classList.add('is-active');
+    });
+
+    // add -is-active classs to all .poster-list elements
+    posterLists.forEach( function(posterlist) {
+      posterlist.classList.add('is-active');
+    });
+
+    // add -is-active classs to the active .poster-wrapper element
   	posterDiv.classList.add('is-active'); //
   })
 
   highlight.addEventListener('mouseout', function() {
-    description.classList.remove('is-active');
-    posterList.classList.remove('is-active');
+    // remove .is-active classs from all .description elements
+    descriptions.forEach( function(description) {
+      description.classList.remove('is-active');
+    });
+
+    // remove .is-active classs from all .poster-list elements
+    posterLists.forEach( function(posterlist) {
+      posterlist.classList.remove('is-active');
+    });
+
+    // remove .is-active classs from the active .poster-wrapper element
   	posterDiv.classList.remove('is-active'); //
   })
 
